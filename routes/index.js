eb312574ad50a0ca6be086a554d7fb08153c3d54
Poke Code <90790144+aucoats@users.noreply.github.com>
2022-02-07 13:08:06 -0500

    pizza model, controllers, routes; CRUD functional

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
+// import all api routes from /api/index.js (no need for index.js since it is implied)
+const apiRoutes = require('./api');
 const htmlRoutes = require('./html/html-routes');
 
+// prefix of `/api` to all api routes imported from api directory
+router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
 router.use((req, res) => {
